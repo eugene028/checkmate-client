141264b2cfdfb2c9b2bfb7e950e85303be14ad82
Eugene Kim <eugene02876@gmail.com>
2024-06-03 23:59:12 +0900

    feat: NavigationBar 구현

--- checkmate/src/components/DatePicker/index.tsx
+++ checkmate/src/components/DatePicker/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import dayjs from "dayjs";
-import { TextField, InputAdornment, IconButton } from "@mui/material";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -24,7 +23,10 @@ const MainPageDatePicker = () => {
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <MobileDatePicker
           sx={{
-            height: 40,
+            "& .MuiOutlinedInput-root": { height: 40, borderColor: "#FBB1B0" },
+            "& .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#FBB1B0", // 아웃라인 색상 변경
+            },
           }}
           label="날짜 선택"
           defaultValue={dayjs(new Date())}
---
+++ checkmate/src/components/Navigation/index.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+const Navigation = () => {
+  return <NavigationContainer />;
+};
+
+const NavigationContainer = styled.div`
+  width: 100%;
+  height: 5rem;
+  border-top: 1px solid "#D9d9d9";
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+`;
